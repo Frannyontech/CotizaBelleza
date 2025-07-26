17c0a738cfb8661f2e4a0cc38c366acaf79f92fa
Frannyontech <fran.galazojeda@gmail.com>
2025-07-26 18:08:35 -0400

    create serializers for models and using fields all to include fields

--- core/serializers.py
+++ core/serializers.py
@@ -1,14 +1,58 @@
 from rest_framework import serializers
-from .models import Producto, Categoria
+from django.contrib.auth.models import User
+from .models import (
+    Producto, Categoria, Tienda, PrecioProducto, 
+    Alerta, AlertaUsuario, Resena
+)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
-        fields = ['id', 'nombre']
+        fields = '__all__'
+
+class TiendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tienda
+        fields = '__all__'
 
 class ProductoSerializer(serializers.ModelSerializer):
     categoria = CategoriaSerializer(read_only=True)
     
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'marca', 'descripcion', 'imagen_url', 'categoria'] 
+        fields = '__all__'
+
+class PrecioProductoSerializer(serializers.ModelSerializer):
+    producto = ProductoSerializer(read_only=True)
+    tienda = TiendaSerializer(read_only=True)
+    
+    class Meta:
+        model = PrecioProducto
+        fields = '__all__'
+
+class ResenaSerializer(serializers.ModelSerializer):
+    producto = ProductoSerializer(read_only=True)
+    usuario = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Resena
+        fields = '__all__'
+
+class AlertaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alerta
+        fields = '__all__'
+
+class AlertaUsuarioSerializer(serializers.ModelSerializer):
+    producto = ProductoSerializer(read_only=True)
+    usuario = UserSerializer(read_only=True)
+    alerta = AlertaSerializer(read_only=True)
+    
+    class Meta:
+        model = AlertaUsuario
+        fields = '__all__' 
